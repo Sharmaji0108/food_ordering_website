692a90f135fa3652e31d4c908bafcc998285f202
Sharmaji0108 <anujsh0108@gmail.com>
2025-06-10 16:28:20 +0530

    Add files via upload

---
+++ Food_ordering_website_backend/src/main/java/com/mini_project/Food_ordering_website_backend/FoodOrderingWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mini_project.Food_ordering_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOrderingWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOrderingWebsiteBackendApplication.class, args);
+	}
+
+}
